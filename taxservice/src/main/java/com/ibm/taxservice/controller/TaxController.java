3cc474772f75d7d9aad6ac4bab9e0774cda28b3e
w2cluster225 <w2cluster225@w2cluster246.iiht.tech>
2020-11-04 15:08:41 +0530

    config file related changes

--- taxservice/src/main/java/com/ibm/taxservice/controller/TaxController.java
+++ taxservice/src/main/java/com/ibm/taxservice/controller/TaxController.java
@@ -1,23 +1,35 @@
 package com.ibm.taxservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.taxservice.service.TaxService;
 
+@RefreshScope
 @RestController
 public class TaxController {
 	
 	@Autowired
 	TaxService taxService;
 	
+	@Value("${message}")
+	private String message;
+	
 	
 	@GetMapping("/tax/{name}")
 	public Double tax(@PathVariable(value="name") String name) {
 		
 	return taxService.tax(name);
+	}
+	
+	@GetMapping("/message")
+	public String msg() {
+		
+		return this.message;
 	}
 
 }
